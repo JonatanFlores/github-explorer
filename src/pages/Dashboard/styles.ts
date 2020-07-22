3082617151643572266d6e136d5c05ea160de1cf
Jonatan Flores <jonatafloress@gmail.com>
2020-07-22 11:33:27 -0300

    created dashboard page styles and layout

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,6 +1,95 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
 
 export const Title = styled.h1`
-  font-size: 48px;
   color: #3a3a3a;
+  font-size: 48px;
+  line-height: 56px;
+  max-width: 450px;
+  margin-top: 80px;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  margin-top: 40px;
+  max-width: 700px;
+
+  input {
+    border: 0;
+    border-radius: 5px 0 0 5px;
+    color: #3a3a3a;
+    flex: 1;
+    height: 70px;
+    padding: 0 24px;
+
+    &::placeholder {
+      color: #a8a8b3;
+    }
+  }
+
+  button {
+    background: #04d361;
+    border: 0;
+    border-radius: 0 5px 5px 0;
+    color: #fff;
+    font-weight: bold;
+    height: 70px;
+    width: 210px;
+    transition: background-color 0.2s;
+
+    &:hover {
+      background: ${shade(0.2, '#04d361')};
+    }
+  }
+`;
+
+export const Repositories = styled.div`
+  margin-top: 80px;
+  max-width: 700px;
+
+  a {
+    align-items: center;
+    background: #fff;
+    border-radius: 5px;
+    display: flex;
+    padding: 24px;
+    text-decoration: none;
+    width: 100%;
+    transition: transform 0.2s;
+
+    & + a {
+      margin-top: 16px;
+    }
+
+    &:hover {
+      transform: translateX(10px);
+    }
+
+    img {
+      border-radius: 50%;
+      height: 64px;
+      width: 64px;
+    }
+
+    div {
+      margin-left: 16px;
+
+      strong {
+        color: #3d3d4d;
+        font-size: 24px;
+        line-height: 28px;
+      }
+
+      p {
+        color: #a8a8b3;
+        font-size: 18px;
+        margin-top: 4px;
+      }
+    }
+
+    svg {
+      color: #cbcbd6;
+      margin-left: auto;
+    }
+  }
 `;
